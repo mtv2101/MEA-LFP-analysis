e5e3f39a8cb90820c788af72756abfa095fdddcb
Matt Valley <mtv2101@columbia.edu>
2010-04-14 16:50:34 -0400

    change math in pmtmprocess base_mode=1 normalize using stdev of baseline, not mean

--- spectral analysis/pmtmprocess.m
+++ spectral analysis/pmtmprocess.m
@@ -1,8 +1,13 @@
 function [spec_norm aveallgamma] = pmtmprocess(spec,f,brthindx,base_mode,g_freqs);
-% spec = (time, freq, trial, breath, channel(n))
 
-baseline = find(brthindx <= 0); %get all breaths before stimilus (stim triggered on breath 0, so event occurs on next breath)
-signal = find(brthindx >= 1); %get all breaths after stimilus
+% Do signal normalization
+% spec input is the pmtm spectrogram from one odor trial from one electrode site
+% spec = (time, freq, trial, breath) for each channel(n)
+% spec_norm is same size as spec
+
+baseline = find(brthindx <= 1); %get all breaths before stimilus (stim triggered on breath 0, so event occurs on next breath)
+signal = find(brthindx > 1); %get all breaths after stimilus
+spec_norm = zeros(size(spec));
 %% Normalize Spectral density by frequency (over all trials), expresses as Db
 
 if base_mode == 0 %base_mode=0 normalizes to average baseline power in each band individaully
@@ -14,17 +19,19 @@ if base_mode == 0 %base_mode=0 normalizes to average baseline power in each band
         spec_norm(:,i,:,:) = 10.*(log10(spec(:,i,:,:)./fmean(i))); %normalize freq band to ave power of baseline of all trials - express as dB change
     end
 
-elseif base_mode == 1 %base_mode=1 normalizes to average baseline power in each band individaully, and sets all baseline specs to zero
+elseif base_mode == 1 %base_mode=1 normalizes to average baseline power to the stdev of each frequencies baseline, and sets all baseline specs to zero
     for i=1:size(spec,2); %iterate over frequency dim
         a = squeeze(mean(spec(:,i,:,:),1)); %average power for each frequency band (ave over time)
-        b = squeeze(mean(a(:,baseline),2)); %average this over baseline breaths
-        fmean(i) = mean(b); %average over all trials
-        spec_norm_a(:,i,:,:) = spec(:,i,:,:)-fmean(i);
-        spec_norm(:,i,:,:) = 10.*(log10((spec_norm_a(:,i,:,:)./fmean(i))+100)); %normalize freq band to ave power of baseline of all trials - express as dB change
-        c = squeeze(mean(spec_norm(:,i,:,:),1)); %average power for each frequency band (ave over time)
-        d = squeeze(mean(c(:,baseline),2)); %average this over baseline breaths
-        fnorm_mean(i) = mean(d); %average over all trials
-        spec_norm(:,i,:,:) = spec_norm(:,i,:,:)-fnorm_mean(i); %subtract average of baseline region per freq, bring baseline average to zero
+            b = squeeze(mean(a(:,baseline),2)); %average this over baseline breaths
+            fmean(i) = mean(b); %average over all trials
+        spec_norm1(:,i,:,:) = spec(:,i,:,:)-fmean(i); %subtract each value from the baseline pop average at that frequency
+            std1 = spec_norm(:,i,:,baseline);
+            base_std(i) = std(std1(:));
+        spec_norm2(:,i,:,:) = 10.*(log10((spec_norm1(:,i,:,:)./base_std(i)))); %normalize freq band to baseline pop stdev - express as dB change
+            c = squeeze(mean(spec_norm2(:,i,:,:),1)); %average power for each frequency band (ave over time)
+            d = squeeze(mean(c(:,baseline),2)); %average this over baseline breaths
+        fnorm_mean(i) = mean(d); %average over all trials, this is a second calculation of the baseline pop average, just after normalization
+        spec_norm(:,i,:,:) = spec_norm2(:,i,:,:)-fnorm_mean(i); %subtract average of baseline region per freq, bring baseline average to zero
     end
 
 elseif base_mode == 2 %base_mode=2 normalizes to max baseline power of all gamma freq bands
