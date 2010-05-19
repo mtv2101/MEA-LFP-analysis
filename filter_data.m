692f5f229b8f2b1618988979fa0cbb7f802ea88f
unknown <Armen@.(none)>
2010-05-18 23:53:46 -0400

    Rewrite of filter_data.m. Avoids all nested for loops, achieving 10x speed increase

--- filter_data.m
+++ filter_data.m
@@ -1,14 +1,19 @@
-function data_filtered= filter_data(data,sampFreq,filttype,filtorder)
-
 %this function filters the data using SinoFilt function. The inputs are data, which as a 4-d matrix dividing the signal into breaths and events, sampFreq, which
 %in our case is 3051.8, and filttype- specifying the desired frequency
 %band which for gamma, will always be 2
 %the output is data_filtered, which is the filtered data in a 4-d matrix
+%-----comment above by Matt Valley
 
-for Chan =1:length(data(1,1,1,:))                       %go through all channels
-    for i=1:length(data(1,:,1,1))                       %go through all events
-        for j=1:length(data(1,1,:,1))                   %go through all breaths
-            data_filtered(:,i,j,Chan)=SinoFilt(data(:,i,j,Chan),sampFreq,filttype,filtorder);    %filter the data
-        end
-    end
-end
+%Armen Enikolopov 5/18/2010
+%  rewrote this so as to avoid 3 nested for loops 
+%  by reshaping 4D matrix to a 2D matrix
+%  a ~10x speed increase is achieved when used on current data.
+
+function data_filtered= filter_data(data,sampFreq,filttype,filtorder)
+    orig_size = size(data);
+    % new shape is [time x (event*breath*chan)]
+    size2D = [orig_size(1), prod(orig_size(2:end))];
+    data2D = reshape(data, size2D);
+    data2D_filtered = SinoFilt(data2D,sampFreq,filttype,filtorder);
+    data_filtered = reshape(data2D_filtered,orig_size);
+ end
